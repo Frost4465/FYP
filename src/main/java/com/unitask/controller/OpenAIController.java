1d75274ae7b0c462676cba3871774015a727fadd
JinMinATF <quahjinmin@altecflex.com>
2025-08-04 00:45:50 +0800

    Change RequestPart to RequestBody

--- src/main/java/com/unitask/controller/OpenAIController.java
+++ src/main/java/com/unitask/controller/OpenAIController.java
@@ -5,10 +5,7 @@ import com.unitask.dto.AIRequest;
 import com.unitask.service.OpenAIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/OpenAI")
@@ -19,7 +16,7 @@ public class OpenAIController {
 
 
     @PostMapping("/chatbot")
-    public ResponseEntity<?> callChatbot(@RequestPart AIRequest request) throws Exception {
+    public ResponseEntity<?> callChatbot(@RequestBody AIRequest request) throws Exception {
         return ResponseEntity.ok().body(openAIService.getResponse(request));
     }
 
